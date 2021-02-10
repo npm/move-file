0388f6ac4de69a167b70773449735ee4c65d0f33
nlf <quitlahok@gmail.com>
2021-02-10 11:46:29 -0800

    also use the copy fallback for EPERM errors
    PR-URL: https://github.com/npm/move-file/pull/5
    Credit: @nlf
    Close: #5
    Reviewed-by: @wraithgar

--- test/async.js
+++ test/async.js
@@ -81,7 +81,71 @@ t.test('move a file across devices', async t => {
   t.equal(fs.readFileSync(dest, 'utf8'), fixture)
 })
 
+t.test('move a file across devices (EPERM)', async t => {
+  const exdevError = new Error()
+  exdevError.code = 'EPERM'
+  const moveFile = requireInject('../', {
+    fs: {
+      ...fs,
+      rename: (s, d, cb) => process.nextTick(() => cb(exdevError)),
+    },
+  })
+
+  const dir = t.testdir({
+    src: fixture,
+  })
+  const dest = `${dir}/dest`
+  await moveFile(`${dir}/src`, dest)
+  t.equal(fs.readFileSync(dest, 'utf8'), fixture)
+})
+
 t.test('move a directory across devices', async t => {
+  const exdevError = new Error()
+  exdevError.code = 'EXDEV'
+  const moveFile = requireInject('../', {
+    fs: {
+      ...fs,
+      rename: (s, d, cb) => process.nextTick(() => cb(exdevError)),
+    },
+  })
+
+  const dir = t.testdir({
+    src: {
+      one: fixture,
+      two: fixture,
+      sub: {
+        three: fixture,
+        four: fixture,
+        five: t.fixture('symlink', './four'),
+        reallysub: {
+          six: t.fixture('symlink', '../../one')
+        }
+      },
+      link: t.fixture('symlink', './sub'),
+      abs: t.fixture('symlink', process.cwd())
+    }
+  })
+  const dest = `${dir}/dest`
+  await moveFile(`${dir}/src`, dest)
+  t.ok(fs.statSync(dest).isDirectory(), 'created a directory')
+  t.equal(fs.readFileSync(`${dest}/one`, 'utf8'), fixture, 'copied file one')
+  t.equal(fs.readFileSync(`${dest}/two`, 'utf8'), fixture, 'copied file two')
+  t.ok(fs.statSync(`${dest}/sub`).isDirectory(), 'created the subdirectory')
+  t.equal(fs.readFileSync(`${dest}/sub/three`, 'utf8'), fixture, 'copied file three')
+  t.equal(fs.readFileSync(`${dest}/sub/four`, 'utf8'), fixture, 'copied file four')
+  t.ok(fs.lstatSync(`${dest}/sub/five`).isSymbolicLink(), 'created a file symbolic link')
+  t.equal(fs.readlinkSync(`${dest}/sub/five`).replace(/\\/g, '/'), './four', 'created file symlink')
+  t.ok(fs.lstatSync(`${dest}/link`).isSymbolicLink(), 'created a directory symbolic link')
+  // below assertion varies for windows because junctions are absolute paths
+  t.equal(fs.readlinkSync(`${dest}/link`), process.platform === 'win32' ? join(dest, 'sub\\') : './sub', 'created the directory symbolic link with the correct target')
+  t.ok(fs.lstatSync(`${dest}/sub/reallysub`).isDirectory(), 'created the innermost subdirectory')
+  t.ok(fs.lstatSync(`${dest}/sub/reallysub/six`).isSymbolicLink(), 'created the innermost symlink')
+  t.equal(fs.readlinkSync(`${dest}/sub/reallysub/six`).replace(/\\/g, '/'), '../../one', 'created the symlink with the appropriate target')
+  t.ok(fs.lstatSync(`${dest}/abs`).isSymbolicLink(), 'created the absolute path symlink')
+  t.equal(fs.readlinkSync(`${dest}/abs`), process.platform === 'win32' ? `${process.cwd()}\\` : process.cwd(), 'kept the correct absolute path')
+})
+
+t.test('move a directory across devices (EPERM)', async t => {
   const exdevError = new Error()
   exdevError.code = 'EXDEV'
   const moveFile = requireInject('../', {
